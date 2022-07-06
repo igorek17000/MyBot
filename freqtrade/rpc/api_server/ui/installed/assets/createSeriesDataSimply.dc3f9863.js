96dfdde93580dcbc9ddb61b489033da2d061dce4
Hemir Muñoz Giraldo <90068980+hemirmg@users.noreply.github.com>
2022-07-06 12:46:35 -0500

    Delete freqtrade directory

--- freqtrade/rpc/api_server/ui/installed/assets/createSeriesDataSimply.dc3f9863.js
+++
@@ -1,2 +0,0 @@
-import{M as t,R as c,bf as m,aY as o}from"./install.0dc8b161.js";function S(a,e,n){e=t(e)&&{coordDimensions:e}||c({encodeDefine:a.getEncode()},e);var r=a.getSource(),s=m(r,e).dimensions,i=new o(s,a);return i.initData(r,n),i}export{S as c};
-//# sourceMappingURL=createSeriesDataSimply.dc3f9863.js.map
--- freqtrade/rpc/api_server/ui/installed/assets/numberformat.a1b2908a.js
+++
@@ -1,2 +0,0 @@
-function t(n){return n==null}function o(n,r=3){return t(n)?"":`${(n*100).toFixed(r)}%`}function e(n,r=15){return t(n)?"N/A":n.toLocaleString("fullwide",{useGrouping:!1,maximumFractionDigits:r})}function u(n,r,i=3){return`${e(n,i)} ${r}`}export{u as a,e as b,o as f};
-//# sourceMappingURL=numberformat.a1b2908a.js.map
