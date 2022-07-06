00aa01989a2b5822ec286b212263386b5fcc39b1
Hemir Muñoz Giraldo <90068980+hemirmg@users.noreply.github.com>
2022-07-06 12:24:41 -0500

    Add files via upload

---
+++ freqtrade/rpc/api_server/ui/installed/assets/LoginView.404aef06.js
@@ -0,0 +1,2 @@
+import{d as r,H as o,n as i}from"./index.ef7539b9.js";var _=r({name:"LoginView",components:{Login:o}}),s=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",{staticClass:"container"},[t("b-card",{attrs:{header:"Freqtrade bot Login"}},[t("Login",{ref:"loginForm",attrs:{id:"loginForm"}})],1)],1)},c=[];const a={};var l=i(_,s,c,!1,v,"a7baf2c8",null,null);function v(n){for(let e in a)this[e]=a[e]}var u=function(){return l.exports}();export{u as default};
+//# sourceMappingURL=LoginView.404aef06.js.map
